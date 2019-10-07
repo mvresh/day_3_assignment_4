ecc416d58e4fb25b9faec9d39fdc43848113875f
Varun Resh Maddali <mvresh@gmail.com>
2019-10-07 16:01:23 +0530

    done the orientation and other tasks

--- lib/main.dart
+++ lib/main.dart
@@ -1,140 +1,210 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      appBar: AppBar(
-        title: Text('Strawberry Pavlova'),
-      ),
-      body: Row(
-        children: <Widget>[
-          Expanded(
-            flex: 1,
-            child: Padding(
-              padding: const EdgeInsets.all(18.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+      body: RecipeOrientation(),
+    ),
+  ));
+}
+
+class RecipeOrientation extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    if(MediaQuery.of(context).orientation == Orientation.portrait)
+    {
+      return portrait();
+    }
+    else {
+      return landscape();
+    };
+  }
+}
+
+Widget landscape() {
+  return new Row(
+    crossAxisAlignment: CrossAxisAlignment.stretch, // this I missed till now
+    children: <Widget>[
+      Expanded(
+        flex: 3,
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(4, 6, 4, 6),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(height:20),
+              Text('Strawberry Pavlova'),
+              SizedBox(height:20),
+              Text(
+                'Pavlova is a meringue-based dessert named after the Russian ballerina Anna Pavlova.'
+                    ' It is a meringue dessert with a crisp crust and soft, light inside,'
+                    ' usually topped with fruit and whipped cream.',textAlign: TextAlign.center,),
+              SizedBox(height:20),
+              Wrap(
                 children: <Widget>[
-                  Expanded(
-                    flex: 1,
-                    child: Center(
-                      child: Text('Strawberry Pavlova',style: TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold,
-                      ),),
-                    ),
+                  Icon(
+                    Icons.star,
+                    size: 12,
                   ),
-                  Expanded(
-                    flex: 4,
-                    child: Padding(
-                      padding: const EdgeInsets.all(4.0),
-                      child: Text('Pavlova is a meringue-based dessert named after the Russian ballerina Anna Pavlova. It is a meringue dessert with a crisp crust and soft, light inside, usually topped with fruit and whipped cream.',
-                        style: TextStyle(
-                          fontSize: 10,
-                        ),),
-                    ),
+                  Icon(
+                    Icons.star,
+                    size: 12,
                   ),
-                  Expanded(
-                    flex:1,
-                    child: Padding(
-                      padding: const EdgeInsets.all(2.0),
-                      child: Row(
-                        children: <Widget>[
-                          Expanded(
-                            child: Padding(
-                              padding: const EdgeInsets.all(2.0),
-                              child: Row(
-                                children: <Widget>[
-                                  Icon(Icons.star,size:12,),
-                                  Icon(Icons.star,size:12,),
-                                  Icon(Icons.star,size:12,),
-                                  Icon(Icons.star,size:12,),
-                                  Icon(Icons.star,size:12,),
-                                ],
-                              ),
-                            ),
-                          ),
-                          Expanded(child: Padding(
-                            padding: const EdgeInsets.all(2.0),
-                            child: Text('170 reviews',
-                              style: TextStyle(
-                                fontSize: 10,
-                              ),),
-                          )),
-                        ],
+                  Icon(
+                    Icons.star,
+                    size: 12,
+                  ),
+                  Icon(
+                    Icons.star,
+                    size: 12,
+                  ),
+                  Icon(
+                    Icons.star,
+                    size: 12,
+                  ),
+                  Text('170 reviews'),
+                ],
+              ),
+              SizedBox(height:20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      Icon(Icons.timer, size: 10, color: Colors.green),
+                      Text(
+                        'COOK:',
                       ),
-                    ),
+                      Text(
+                        '1 hr',
+                      )
+                    ],
                   ),
-                  Expanded(
-                    flex: 4,
-                    child: Row(
-                      children: <Widget>[
-                        Expanded(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Icon(Icons.book,size:10,color: Colors.green,),
-                              SizedBox(height: 6,),
-                              Text('PREP:',
-                              style: TextStyle(
-                                fontSize: 10,
-                              ),),
-                              SizedBox(height: 12,),
-                              Text('25 min.',
-                                style: TextStyle(
-                                  fontSize: 10,
-                                ),),
-                            ],
-                          ),
-                        ),
-                        Expanded(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Icon(Icons.timer,size:10,color: Colors.green),
-                              SizedBox(height: 6,),
-                              Text('COOK:',
-                                style: TextStyle(
-                                  fontSize: 10,
-                                ),),
-                              SizedBox(height: 12,),
-                              Text('1 hr',
-                                style: TextStyle(
-                                  fontSize: 10,
-                                ),),
-                            ],
-                          ),
-                        ),
-                        Expanded(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Icon(Icons.restaurant,size:10,color: Colors.green),
-                              SizedBox(height: 6,),
-                              Text('FEEDS:',
-                                style: TextStyle(
-                                  fontSize: 10,
-                                ),),
-                              SizedBox(height: 12,),
-                              Text('4-6 hr',
-                                style: TextStyle(
-                                  fontSize: 10,
-                                ),),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
+                  Column(
+                    children: <Widget>[
+                      Icon(Icons.timer, size: 10, color: Colors.green),
+                      Text(
+                        'COOK:',
+                      ),
+                      Text(
+                        '1 hr',
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Icon(Icons.timer, size: 10, color: Colors.green),
+                      Text(
+                        'COOK:',
+                      ),
+                      Text(
+                        '1 hr',
+                      )
+                    ],
                   ),
                 ],
               ),
-            ),
+            ],
           ),
-          Expanded(
-              flex: 2,
-              child: Image(image:AssetImage('assets/Strawpav.jpg'))),
-        ],
+        ),
       ),
-    ),
-  ));
+      Expanded(
+          flex: 7,
+          child: Image(image: AssetImage('assets/Strawpav.jpg'),fit: BoxFit.fitHeight,)),
+    ],
+  );
+}
+
+Widget portrait(){
+  return new Column(
+    children: <Widget>[
+      Expanded(
+          flex: 1,
+          child: Image(image: AssetImage('assets/Strawpav.jpg'),fit: BoxFit.fitHeight,)),
+      Expanded(
+        flex: 1,
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(4, 6, 4, 6),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(height:20),
+              Text('Strawberry Pavlova'),
+              SizedBox(height:20),
+              Text(
+                'Pavlova is a meringue-based dessert named after the Russian ballerina Anna Pavlova.'
+                    ' It is a meringue dessert with a crisp crust and soft, light inside,'
+                    ' usually topped with fruit and whipped cream.',textAlign: TextAlign.center,),
+              SizedBox(height:20),
+              Wrap(
+                children: <Widget>[
+                  Icon(
+                    Icons.star,
+                    size: 12,
+                  ),
+                  Icon(
+                    Icons.star,
+                    size: 12,
+                  ),
+                  Icon(
+                    Icons.star,
+                    size: 12,
+                  ),
+                  Icon(
+                    Icons.star,
+                    size: 12,
+                  ),
+                  Icon(
+                    Icons.star,
+                    size: 12,
+                  ),
+                  Text('170 reviews'),
+                ],
+              ),
+              SizedBox(height:20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      Icon(Icons.timer, size: 10, color: Colors.green),
+                      Text(
+                        'COOK:',
+                      ),
+                      Text(
+                        '1 hr',
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Icon(Icons.timer, size: 10, color: Colors.green),
+                      Text(
+                        'COOK:',
+                      ),
+                      Text(
+                        '1 hr',
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Icon(Icons.timer, size: 10, color: Colors.green),
+                      Text(
+                        'COOK:',
+                      ),
+                      Text(
+                        '1 hr',
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      )
+    ],
+  );
 }
